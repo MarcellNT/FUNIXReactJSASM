c06c8b4ccdcf65956bac88c58d36f6c6f4818fde
MarcellNT <100115775+MarcellNT@users.noreply.github.com>
2022-06-13 23:49:52 +0700

    Use BorBrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Main from "./component/MainComponent";
-
-
+import { BrowserRouter } from "react-router-dom";
+// Su dung BrowserRouter su dung bo dinh tuyen History API cua HTML5 de giu giao dien nguoi dung dong bo voi URL
+// BrowserRouter la thanh phan me luu tru bao tat ca cac thanh phan khac ben trong
 function App() {
   return (
+    <BrowserRouter>
       <div className="wrapper">
         <Main />
-      </div>  
+      </div>
+    </BrowserRouter>
   )
 }
 
